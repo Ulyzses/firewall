3d71bdcedbc9a8ac4bae0965f79a4ac1969f0cfa
Marius Barcenas <mariusulyzses.barcenas@gmail.com>
2024-05-29 13:30:33 +0800

    api: add ping POST endpoint

--- server/src/routes/api/ping/+server.ts
+++ server/src/routes/api/ping/+server.ts
@@ -1,3 +1,11 @@
-export function GET() {
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async () => {
   return new Response('Pong!', { status: 200 });
+}
+
+export const POST: RequestHandler = async ({ request }) => {
+  const body = await request.json();
+  return json({ message: 'Pong!', body });
 }
