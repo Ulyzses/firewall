827939faddbff8bd4f6e4eb24cebe38ed92a0e75
jdy <96467690+jdy38@users.noreply.github.com>
2024-05-30 01:30:12 +0800

    feat: add to dashboard ui

--- server/src/routes/dashboard/+page.svelte
+++ server/src/routes/dashboard/+page.svelte
@@ -12,30 +12,33 @@
     body: 'Provide the MAC address of your device below, e.g. 202111579, 202112345',
     // Populates the input value and attributes
     value: '',
-    valueAttr: { type: 'number', minlength: 10, maxlength: 10, required: true },
+    valueAttr: { type: 'text', minlength: 9, maxlength: 9, required: true },
     // Returns the updated response value
     response: (r: string) => {
       addDeviceValue = r;
       console.log('response:', r);
-    },
+    }
   };
-
+  
   let addDeviceValue = '';
+
+  const statusColors = ['bg-green-500', 'bg-orange-400', 'bg-firewall-red'];
+  const statusLabels = ['ON', 'FORCED OFF', 'OFF'];
               
   type Device = {
     name: string;
     id: number;
-    status: string;
+    status: number;
     smoke: boolean;
   };
 
   let devices: Device[] = [
-    { name: 'pips', id: 202111579, status: 'on', smoke: true },
-    { name: 'lance', id: 201800866, status: 'on', smoke: false },
-    { name: 'julia', id: 202108595, status: 'forced off', smoke: false },
-    { name: 'renzo', id: 202100912, status: 'forced off', smoke: false },
-    { name: 'marius', id: 202102371, status: 'off', smoke: false },
-    { name: 'jaren', id: 202102804, status: 'off', smoke: false }
+    { name: 'pips', id: 202111579, status: 0, smoke: true },
+    { name: 'lance', id: 201800866, status: 0, smoke: false },
+    { name: 'julia', id: 202108595, status: 1, smoke: false },
+    { name: 'renzo', id: 202100912, status: 1, smoke: false },
+    { name: 'marius', id: 202102371, status: 2, smoke: false },
+    { name: 'jaren', id: 202102804, status: 2, smoke: false }
   ];
 
   export let data;
@@ -45,8 +48,11 @@
   let addDeviceForm: HTMLFormElement;
 
   async function addDevice() {
+    console.log(addDeviceValue);
     const formData = new FormData(addDeviceForm);
     const macAddress = formData.get('macAddress') as string;
+
+    console.log(macAddress);
 
     const { data, error } = await supabase
       .from('devices')
@@ -64,27 +70,30 @@
   }
 </script>
 
-<nav class='flex flex-row justify-between h-min w-full mb-6 mx-6'>
-  <p class='text-lg' style='color: #E7191F;'>FireWall</p>
-  <p class='text-lg color-black italic'>Hello, user!</p>
-  <!-- user icon -->
-    <!-- logout -->
+<nav class='grid grid-flow-row grid-cols-2 justify-between h-min w-auto mb-6 mx-6'>
+  <p class='text-2xl text-firewall-red font-kollektif'>FireWall</p>
+  <div class='text-mg text-black italic text-right font-DM'>
+    Hello, user!
+  </div>
 </nav>
 <div class='grid grid-flow-row grid-cols-1 lg:grid-cols-2 gap-4 overflow-y-scroll mx-6'>
   <div class='flex flex-row justify-between w-full h-min lg:col-span-2 items-center'>
-    <h1 class='text-3xl'>Devices</h1>
+    <h1 class='text-3xl font-kollektif'>Devices</h1>
     <form bind:this={addDeviceForm} on:submit|preventDefault={addDevice}>
       <label for="macAddress">
-        <!-- CHANGE THIS -->
-        <input name="macAddress" type="hidden" bind:value={addDeviceValue}/>
+        <input name="macAddress" type="hidden" bind:value={ addDeviceValue }/>
       </label>
-      <button class='bg-red-400 p-4 rounded-lg w-40 h-min text-sm text-white' on:click={() => {modalStore.trigger(modalAddDevice);}}>+ Add Device</button>
+      <button class='bg-firewall-red p-4 rounded-lg w-40 h-min text-sm text-white font-DM' on:click={() => { modalStore.trigger(modalAddDevice); }}>+ Add Device</button>
     </form>
   </div>
   {#each devices as device (device.id)}
-    <div class='w-auto h-40 p-4 items-center bg-gray-200 rounded-lg border border-gray-200'>
-      <p class='w-min h-min'>{device.name}</p>
-      <p class='w-min h-min'>{device.id}</p>
+    <div class='flex flex-row flex-none w-auto h-40 p-8 items-center bg-gray-200 rounded-lg border border-gray-200 font-DM gap-6 justify-between'>
+      <p>plug img</p>
+      <div class='h-auto w-1/4 grid grid-flow-cols justify-items-left justify-self-center'>
+        <p class='w-min h-min'>{ device.name }</p>
+        <p class='w-min h-min'>{ device.id }</p>
+      </div>
+      <button class='h-full w-1/4 { statusColors[device.status] } text-white rounded-lg'>{ statusLabels[device.status] }</button>
     </div>
   {/each}
 </div>
--- server/tailwind.config.js
+++ server/tailwind.config.js
@@ -5,6 +5,8 @@ import { join } from 'path';
 // 1. Import the Skeleton plugin
 import { skeleton } from '@skeletonlabs/tw-plugin';
 
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	// 2. Opt for dark mode to be handled via the class method
@@ -18,7 +20,15 @@ export default {
 		)
 	],
 	theme: {
-		extend: {},
+		extend: {
+      colors: {
+        'firewall-red': '#e7191f',
+      },
+      fontFamily: {
+        DM: ["DM Sans", "sans-serif"],
+        kollektif: ["Kollektif", 'sans-serif'],
+      },
+    },
 	},
 	plugins: [
 		// 4. Append the Skeleton plugin (after other plugins)
