df379a2930a2db503a3b9dd355bc667f1cb2e86d
Marius Barcenas <mubarcenas@up.edu.ph>
2024-05-30 17:37:52 +0800

    fix: update database when action is sent to user (potentially breaking) (#14)

--- server/src/routes/api/status/+server.ts
+++ server/src/routes/api/status/+server.ts
@@ -4,6 +4,7 @@ import type { RequestHandler } from '@sveltejs/kit';
 export const GET: RequestHandler = async ({ url, locals: { supabase } }) => {
   const macAddress = url.searchParams.get('m') as string;
   const state = Number(url.searchParams.get('s')) as number;
+  const first = Number(url.searchParams.get('f')) as number;
 
   if (!macAddress) {
     return text('MAC Address is required', { status: 400 });
@@ -29,6 +30,11 @@ export const GET: RequestHandler = async ({ url, locals: { supabase } }) => {
     return text('Device not found', { status: 404 });
   }
 
+  // If first request, return last status of the device
+  if (first === 1) {
+    return text(`${doesExist.data.status ? 'ON' : 'OFF'}`);
+  }
+
   // Get the latest action (if any) for the device within the last 5 minutes
   const queuedAction = await supabase
     .from('actions')
@@ -52,6 +58,16 @@ export const GET: RequestHandler = async ({ url, locals: { supabase } }) => {
     if ((queuedAction.data.action === "ON" && state === 1) ||
         (queuedAction.data.action === "OFF" && state === 0)) {
       return new Response(null, { status: 204 });
+    }
+
+    const updateDevice = await supabase
+      .from('devices')
+      .update({ status: queuedAction.data.action === "ON" ? 1 : 0 })
+      .eq('mac', macAddress)
+      .eq('connected', true);
+    
+    if (updateDevice.error) {
+      return text(`Error: ${updateDevice.error.message}`, { status: 500 });
     }
 
     return text(`${queuedAction.data.action}`);
