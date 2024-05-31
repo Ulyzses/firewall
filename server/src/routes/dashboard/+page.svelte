0687d6f4beabdcdf9d86552a29d5b7709426b351
jdy <96467690+jdy38@users.noreply.github.com>
2024-05-31 17:14:46 +0800

    ui: add modal pop up and button disabling for forced-off devices (#21)
    * ui: add modal pop up and button disabling for forced-off devices
    
    * chore: change 'OK' to 'Close' in button text
    
    * chore: change padding on modal

--- server/src/routes/dashboard/+page.svelte
+++ server/src/routes/dashboard/+page.svelte
@@ -13,7 +13,7 @@
 		macList: string[];
 	};
 
-  const modal: ModalSettings = {
+  const modalAdd: ModalSettings = {
     type: 'component',
     component: modalComponent,
     backdropClasses: '!backdrop-blur-md',
@@ -27,6 +27,15 @@
       }
       console.log(r);
     },
+  };
+
+  const modalAlert: ModalSettings = {
+    type: 'alert',
+    backdropClasses: '!backdrop-blur-md',
+    modalClasses: '!bg-white !rounded-lg !px-8 !py-7',
+    title: 'You cannot turn this device on.',
+    body: 'This device has detected smoke and has been turned off automatically. If you want to turn this device on, please turn it on physically.',
+    buttonTextCancel: 'Close'
   };
   
   let addDeviceMac = '';
@@ -85,7 +94,7 @@
 <div class='grid grid-flow-row grid-cols-1 lg:grid-cols-2 gap-4 mx-6 overflow-y-auto'>
   <div class='flex flex-row justify-between w-full h-min lg:col-span-2 items-center'>
     <h1 class='text-xl sm:text-3xl font-kollektif'>Devices</h1>
-    <button class='border border-white bg-firewall-red p-2 sm:p-4 rounded-lg w-32 sm:w-40 h-min text-sm text-white font-DM hover:border-firewall-red' on:click={() => { modalStore.trigger(modal); }}>+ Add Device</button>
+    <button class='border border-white bg-firewall-red p-2 sm:p-4 rounded-lg w-32 sm:w-40 h-min text-sm text-white font-DM hover:border-firewall-red' on:click={() => { modalStore.trigger(modalAdd); }}>+ Add Device</button>
   </div>
   {#if devices.some((x) => x.smoke)}
     <div class='flex flex-row space-x-2 justify-left items-center text-xs font-bold text-lg py-2 s-auto rounded-lg lg:col-span-2'>
@@ -93,7 +102,7 @@
       <span class='text-firewall-red sm:text-base'>WARNING: One of your devices has detected smoke and has been turned off automatically.</span>
     </div>
   {/if}
-  {#each devices as device (device.id)}
+  {#each devices as device (device.mac)}
     <div class='flex flex-row flex-none w-auto h-min pl-4 pr-2 py-4 sm:p-4 sm:p-8 text-sm sm:text-base items-center bg-gray-200 rounded-lg border border-gray-200 font-DM gap-6 justify-between font-bold'>
       <img src={ plug } alt='' class='size-20 sm:size-24 md:size-32' />
       <div class='basis-1/3 h-auto w-1/4 grid grid-flow-cols justify-items-left justify-self-center'>
@@ -109,8 +118,10 @@
       </div>
       <div class='flex flex-col h-full w-1/4 text-center justify-center items-center md:text-lg'>
         <span class='font-bold { statusColors[Number(device.status)] }'>{ statusLabels[Number(device.status)] }</span>
-        <SlideToggle name="slide" bind:checked={device.status} active="bg-green-500" background='bg-firewall-red' size="md" on:click={() => toggleDevice(device)} />
-        </div>
+        <button on:click={() => {if (device.smoke) {modalStore.trigger(modalAlert)}}}>
+          <SlideToggle name="slide" bind:disabled={device.smoke} bind:checked={device.status} active="bg-green-500" background='bg-firewall-red' size="md" on:click={() => toggleDevice(device)} />
+        </button>
+      </div>
     </div>
   {/each}
 </div>
