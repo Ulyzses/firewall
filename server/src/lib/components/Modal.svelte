fc800c8ba0124fac63074b39bbf47d6fbcbd47ce
jdy <96467690+jdy38@users.noreply.github.com>
2024-05-31 15:29:56 +0800

    feat: change device container ui, add device modal (#20)
    * feat: add formatted 'add device' modal
    
    * feat: change device status button to toggle, add responsiveness
    
    * fix: dashboard device card ui fixes
    
    * chore: change font size of alert on desktop
    
    * ui: minor fixes

---
+++ server/src/lib/components/Modal.svelte
@@ -0,0 +1,56 @@
+<script lang='ts'>
+	// COMPONENT FROM 
+  import type { SvelteComponent } from 'svelte';
+  import { getModalStore } from '@skeletonlabs/skeleton';
+
+  export let parent: SvelteComponent;
+
+  const modalStore = getModalStore();
+
+	const formData = {
+		name: '',
+		macList: ['','','','','','']
+	};
+
+	function onFormSubmit(): void {
+		if ($modalStore[0].response) $modalStore[0].response(formData);
+		modalStore.close();
+	}
+
+	const cBase = 'card p-4 w-modal shadow-xl space-y-4 bg-white font-DM rounded-lg';
+	const cHeader = 'text-2xl font-bold';
+	const cInput = 'p-1 rounded-lg border border-gray-500';
+	const cButton = 'p-2 rounded-lg border border-white'
+
+	const numbers = [0, 1, 2, 3, 4, 5];
+</script>
+
+{#if $modalStore[0]}
+	<div class="modal-example-form {cBase}">
+		<header class={cHeader}>Add New Device</header>
+		<article>Please choose your device's name and input your device's ID below.</article>
+		<!-- Enable for debugging: -->
+		<form class="modal-form space-y-4">
+			<label class="label">
+				<span>Device Name</span>
+				<input class="input {cInput} px-3" type="text" bind:value={formData.name} placeholder="Device Name" />
+			</label>
+			<label class="label">
+				<span>Device ID</span>
+        <div class='flex flex-row space-x-1'>
+					{#each numbers as number (number)}
+						<input class="input {cInput} text-center" type="text" minlength="2" maxlength="2" bind:value={formData.macList[number]} placeholder="00" />
+						{#if number < 5}
+							<span>:</span>
+						{/if}
+					{/each}
+        </div>
+			</label>
+		</form>
+		<!-- prettier-ignore -->
+		<footer class="modal-footer {parent.regionFooter}">
+			<button class="btn {parent.buttonNeutral} p-2 rounded-xl border border-black bg-black text-white hover:bg-white hover:text-black w-24" on:click={parent.onClose}>{parent.buttonTextCancel}</button>
+			<button class="btn {parent.buttonPositive} p-2 rounded-xl border border-firewall-red bg-firewall-red text-white hover:bg-white hover:text-firewall-red w-24" on:click={onFormSubmit}>Submit</button>
+		</footer>
+	</div>
+{/if}
--- server/src/routes/dashboard/+page.svelte
+++ server/src/routes/dashboard/+page.svelte
@@ -2,28 +2,37 @@
   import { getModalStore } from '@skeletonlabs/skeleton';
   import type { ModalSettings, ModalComponent, ModalStore } from '@skeletonlabs/skeleton';
   import plug from '$lib/assets/plug.svg';
+  import addDeviceModalComponent from '$lib/components/Modal.svelte';
+  import { SlideToggle } from '@skeletonlabs/skeleton';
+
+  const modalComponent: ModalComponent = { ref: addDeviceModalComponent };
   const modalStore = getModalStore();
 
-  const modalAddDevice: ModalSettings = {
+  type formData = {
+		name: string;
+		macList: string[];
+	};
+
+  const modal: ModalSettings = {
+    type: 'component',
+    component: modalComponent,
     backdropClasses: '!backdrop-blur-md',
     modalClasses: '!bg-white',
-    type: 'prompt',
-    // Data
-    title: 'Enter Device ID',
-    body: 'Provide the MAC address of your device below, e.g. 34:38:3a:45:37:3a',
-    // Populates the input value and attributes
-    value: '',
-    valueAttr: { type: 'text', minlength: 17, maxlength: 17, required: true },
-    // Returns the updated response value
-    response: (r: string) => {
-      addDeviceValue = r;
-      addDevice();
-    }
+    title: 'Add Device',
+    response(r : formData | undefined | boolean) {
+      if (typeof(r) == 'object'){
+        addDeviceMac = r.macList.join(':');
+        addDeviceName = r.name;
+        addDevice();
+      }
+      console.log(r);
+    },
   };
   
-  let addDeviceValue = '';
+  let addDeviceMac = '';
+  let addDeviceName= '';
 
-  const statusColors = ['bg-firewall-red', 'bg-green-500'];
+  const statusColors = ['text-firewall-red', 'text-green-500'];
   const statusLabels = ['OFF', 'ON'];
               
   interface Device {
@@ -37,14 +46,13 @@
 
   $: ({ user, supabase, devices } = data);
 
-  let addDeviceForm: HTMLFormElement;
-
   async function addDevice() {
     const { data, error } = await supabase
       .from('devices')
       .insert({
         user: user?.id,
-        mac: addDeviceValue
+        mac: addDeviceMac,
+        name: addDeviceName
       })
       .select();
 
@@ -70,40 +78,39 @@
       console.log(data);
     }
 
-    device.status = !device.status;
     devices = devices;
   }
 </script>
 
-<div class='grid grid-flow-row grid-cols-1 lg:grid-cols-2 gap-4 mx-6'>
+<div class='grid grid-flow-row grid-cols-1 lg:grid-cols-2 gap-4 mx-6 overflow-y-auto'>
   <div class='flex flex-row justify-between w-full h-min lg:col-span-2 items-center'>
     <h1 class='text-xl sm:text-3xl font-kollektif'>Devices</h1>
-    <label for="macAddress">
-      <input name="macAddress" type="hidden" bind:value={ addDeviceValue }/>
-    </label>
-    <button class='border border-white bg-firewall-red p-2 sm:p-4 rounded-lg w-32 sm:w-40 h-min text-sm text-white font-DM hover:border-firewall-red' on:click={() => { modalStore.trigger(modalAddDevice); }}>+ Add Device</button>
+    <button class='border border-white bg-firewall-red p-2 sm:p-4 rounded-lg w-32 sm:w-40 h-min text-sm text-white font-DM hover:border-firewall-red' on:click={() => { modalStore.trigger(modal); }}>+ Add Device</button>
   </div>
-  <div class='overflow-y-scroll'>
-    {#each devices as device (device.id)}
-      <div class='flex flex-row flex-none w-auto h-40 p-8 items-center bg-gray-200 rounded-lg border border-gray-200 font-DM gap-6 justify-between font-bold'>
-        <img src={ plug } alt='' class='size-28 md:size-32' />
-        <div class='basis-1/3 h-auto w-1/4 grid grid-flow-cols justify-items-left justify-self-center'>
-          <span>{ device.name }</span>
-          <span class='italic'>{ device.mac }</span>
-          <span class='text-firewall-red text-sm'>         
-            {#if device.smoke}
-              Smoke Detected!
-            {:else}
-              <br>
-            {/if}
-          </span>
-        </div>
-        <button class='h-full w-1/4 { statusColors[Number(device.status)] } text-white rounded-lg text-center border border-gray-200 hover:border-0' on:click={() => toggleDevice(device)}>
-          <span>STATUS:</span>
-          <br>
-          <span class='font-bold'>{ statusLabels[Number(device.status)] }</span>
-        </button>
+  {#if devices.some((x) => x.smoke)}
+    <div class='flex flex-row space-x-2 justify-left items-center text-xs font-bold text-lg py-2 s-auto rounded-lg lg:col-span-2'>
+      <span class='shrink-0 bg-firewall-red text-white text-lg w-8 h-8 rounded-full flex justify-center items-center'>!</span>
+      <span class='text-firewall-red sm:text-base'>WARNING: One of your devices has detected smoke and has been turned off automatically.</span>
+    </div>
+  {/if}
+  {#each devices as device (device.id)}
+    <div class='flex flex-row flex-none w-auto h-min pl-4 pr-2 py-4 sm:p-4 sm:p-8 text-sm sm:text-base items-center bg-gray-200 rounded-lg border border-gray-200 font-DM gap-6 justify-between font-bold'>
+      <img src={ plug } alt='' class='size-20 sm:size-24 md:size-32' />
+      <div class='basis-1/3 h-auto w-1/4 grid grid-flow-cols justify-items-left justify-self-center'>
+        <span class='text-base sm:text-lg w-full truncate'>{ device.name }</span>
+        <span class='italic w-full truncate'>{ device.mac }</span>
+          <span class='text-firewall-red text-xs sm:text-sm w-full truncate'>   
+          {#if device.smoke}
+            Smoke Detected!
+          {:else}
+            <br>
+          {/if}
+        </span>
       </div>
-    {/each}
-  </div>
+      <div class='flex flex-col h-full w-1/4 text-center justify-center items-center md:text-lg'>
+        <span class='font-bold { statusColors[Number(device.status)] }'>{ statusLabels[Number(device.status)] }</span>
+        <SlideToggle name="slide" bind:checked={device.status} active="bg-green-500" background='bg-firewall-red' size="md" on:click={() => toggleDevice(device)} />
+        </div>
+    </div>
+  {/each}
 </div>
